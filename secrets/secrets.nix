a0fa18e84c2839eea06624e2b6e54752d8f4d7e9
uku <uku3lig@gmail.com>
2023-11-15 00:59:51 +0100

    add vesuvio key to secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,11 @@
 let
   main = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+7+KfdOrhcnHayxvOENUeMx8rE4XEIV/AxMHiaNUP8 uku3lig"];
+
+  server = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILdyRFBTdyCCMQ7I75TyO9voxrrreXQTXtSw+iCRf4XI root@vesuvio"] ++ main;
 in {
   "desktop/rootPassword.age".publicKeys = main;
   "desktop/userPassword.age".publicKeys = main;
+
+  "vesuvio/rootPassword.age".publicKeys = server;
+  "vesuvio/userPassword.age".publicKeys = server;
 }
