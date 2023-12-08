2336f688851ae2cfb2ba9c2358d2ca3d5b950900
uku <uku3lig@gmail.com>
2023-12-08 14:47:11 +0100

    remove system vencord patch in vesktop
    FUCK REPRODUCIBILITY YEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA BABYYYYYYYYYYYYYYYY

--- exprs/overlay.nix
+++ exprs/overlay.nix
@@ -1,3 +1,6 @@
 final: prev: {
   shotcut = final.qt6Packages.callPackage ./shotcut.nix {};
+
+  # FUCK REPRODUCIBILITY RARGHGHGHGHDKGJDKLGJSDKLGMDJGLKSDJLMGSJDKMGJZEIZ
+  vesktop = prev.vesktop.overrideAttrs (old: { patches = []; });
 }
