7dcd7136c183a99e3c5a26cd88de295e3d4d4580
uku <uku3lig@gmail.com>
2023-12-19 14:21:11 +0100

    patch electron 25 and mark it as secure :innocent:

--- exprs/overlay.nix
+++ exprs/overlay.nix
@@ -4,12 +4,8 @@ final: prev: {
   # FUCK REPRODUCIBILITY RARGHGHGHGHDKGJDKLGJSDKLGMDJGLKSDJLMGSJDKMGJZEIZ
   vesktop = prev.vesktop.overrideAttrs (old: {patches = [];});
 
-  obsidian = (
-    prev.obsidian.override {
-      electron = prev.electron_25.overrideAttrs (_: {
-        preFixup = "patchelf --add-needed ${prev.libglvnd}/lib/libEGL.so.1 $out/bin/electron"; # NixOS/nixpkgs#272912
-        meta.knownVulnerabilities = []; # NixOS/nixpkgs#273611
-      });
-    }
-  );
+  electron_25 = prev.electron_25.overrideAttrs (_: {
+    preFixup = "patchelf --add-needed ${prev.libglvnd}/lib/libEGL.so.1 $out/bin/electron"; # NixOS/nixpkgs#272912
+    meta.knownVulnerabilities = []; # NixOS/nixpkgs#273611
+  });
 }
