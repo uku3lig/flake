1d191fa1add45b2bfee5437ae1b3424f9bcb0765
uku <uku3lig@gmail.com>
2023-12-24 07:40:13 +0100

    remove shotcut overlay

--- exprs/overlay.nix
+++ exprs/overlay.nix
@@ -1,6 +1,4 @@
 final: prev: {
-  shotcut = final.qt6Packages.callPackage ./shotcut.nix {};
-
   # FUCK REPRODUCIBILITY RARGHGHGHGHDKGJDKLGJSDKLGMDJGLKSDJLMGSJDKMGJZEIZ
   vesktop = prev.vesktop.overrideAttrs (old: {patches = [];});
 
