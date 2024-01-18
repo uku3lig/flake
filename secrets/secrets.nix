479264f533af5d33f6f94826a655079fad1097ac
uku <uku3lig@gmail.com>
2024-01-18 18:32:14 +0100

    switch etna age key
    im gonna explode

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   fuji = "age16ujdfcahmnhe4ygruf28n0urgxycv8zgsp4f8856a5suewhn49cs0mqk7w";
   kilimandjaro = "age1ny0re542mcvf829y28rz6eta9myaqlxasfnn933srw64dlgavpsqc59q79";
-  etna = "age1n5was53x7mn3n7pugt3ge8tg5uglq3hzdt7270a04yw3szrqtuuqy0j6tw";
+  etna = "age1m3jm6c5ywc5zntv5j4xhals0h28mpea88zzddq88zxcshmhteqwqu89qnh";
 
   main = [fuji kilimandjaro];
   all = main ++ [etna];
