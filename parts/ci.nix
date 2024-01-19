5e8208429fc85f08c9ad82be577b9542c3e90ed0
uku <uku3lig@gmail.com>
2024-01-19 01:06:11 +0100

    experimental: new ci

--- parts/ci.nix
+++ parts/ci.nix
@@ -1,45 +1,11 @@
 {
   self,
-  inputs,
+  lib,
   ...
 }: {
-  perSystem = {
-    lib,
-    pkgs,
-    system,
-    ...
-  }: {
-    packages = let
-      overlay = lib.fix (final: (import ../exprs/overlay.nix final pkgs));
-
-      # do not include a package if it's not available on the system or if it's broken
-      isValid = _: v:
-        lib.elem pkgs.system (v.meta.platforms or [pkgs.system]) && !(v.meta.broken or false);
-    in
-      lib.filterAttrs isValid overlay;
+  flake.hydraJobs = let
+    mapCfgsToDerivs = lib.mapAttrs (_: cfg: cfg.activationPackage or cfg.config.system.build.toplevel);
+  in {
+    nixosConfigurations = mapCfgsToDerivs self.nixosConfigurations;
   };
-
-  flake.ghaMatrix.include = let
-    inherit (inputs.nixpkgs) lib;
-
-    platforms = {
-      "x86_64-linux" = {
-        os = "ubuntu-latest";
-      };
-    };
-  in
-    lib.pipe platforms [
-      builtins.attrNames # get systems
-      (systems: lib.getAttrs systems self.packages) # get packages of each system
-      (lib.mapAttrs (_: builtins.attrNames)) # keep only the name of each package
-
-      # map each package in each system to an attrset
-      (lib.mapAttrsToList (system:
-        builtins.map (pkg: {
-          inherit (platforms.${system}) os;
-          pkg = "packages.${system}.${pkg}";
-        })))
-
-      lib.flatten
-    ];
 }
