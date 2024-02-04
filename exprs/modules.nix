7f7225feef8f331bb4621e2545634e838420c38b
uku <uku3lig@gmail.com>
2024-02-04 16:46:24 +0100

    make reposilite a proper package

--- exprs/modules.nix
+++ exprs/modules.nix
@@ -2,4 +2,10 @@
   flake.nixosModules = {
     reposilite = import ./reposilite/module.nix self;
   };
+
+  perSystem = {pkgs, ...}: {
+    packages = {
+      reposilite = pkgs.callPackage ./reposilite/derivation.nix {};
+    };
+  };
 }
