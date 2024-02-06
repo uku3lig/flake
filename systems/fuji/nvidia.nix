9c70ee40aebbdd9a5377835f0a2ff234ea952011
uku <uku3lig@gmail.com>
2024-02-06 14:45:06 +0100

    nvidia: switch back to 545

--- systems/fuji/nvidia.nix
+++ systems/fuji/nvidia.nix
@@ -18,15 +18,6 @@
       enable = true;
       finegrained = false;
     };
-
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "550.40.07";
-      sha256_64bit = "sha256-KYk2xye37v7ZW7h+uNJM/u8fNf7KyGTZjiaU03dJpK0=";
-      sha256_aarch64 = "sha256-AV7KgRXYaQGBFl7zuRcfnTGr8rS5n13nGUIe3mJTXb4=";
-      openSha256 = "sha256-mRUTEWVsbjq+psVe+kAT6MjyZuLkG2yRDxCMvDJRL1I=";
-      settingsSha256 = "sha256-c30AQa4g4a1EHmaEu1yc05oqY01y+IusbBuq+P6rMCs=";
-      persistencedSha256 = "sha256-11tLSY8uUIl4X/roNnxf5yS2PQvHvoNjnd2CB67e870=";
-    };
   };
 
   # boot.extraModprobeConfig = ''
@@ -40,5 +31,6 @@
     "__GLX_VENDOR_LIBRARY_NAME,nvidia"
     "WLR_NO_HARDWARE_CURSORS,1"
     "__EGL_VENDOR_LIBRARY_FILENAMES,/run/opengl-driver/share/glvnd/egl_vendor.d/10_nvidia.json"
+    "NVD_BACKEND,direct"
   ];
 }
