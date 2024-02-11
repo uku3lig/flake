5ebcafb2af1e9d72bb040486766276160c6032bd
uku <uku3lig@gmail.com>
2024-02-11 01:14:29 +0100

    new monitor baybeeeeeeeeeeeeee

--- systems/fuji/default.nix
+++ systems/fuji/default.nix
@@ -10,6 +10,6 @@
   hm = {
     home.packages = with pkgs; [ryujinx];
 
-    wayland.windowManager.hyprland.settings.monitor = lib.mkForce ["DP-1,1920x1200@60,0x0,1" "HDMI-A-1,1440x900@60,1920x300,1"];
+    wayland.windowManager.hyprland.settings.monitor = lib.mkForce ["DP-1,3840x2160@144,0x0,1.5" "HDMI-A-1,1440x900@60,1920x300,1"];
   };
 }
