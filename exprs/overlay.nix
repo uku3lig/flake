758eea0de201a97e7707da68e857c92653436a74
uku <uku3lig@gmail.com>
2024-04-18 18:40:56 +0200

    fix(overlay): bump fuzzel to 1.10.2

--- exprs/overlay.nix
+++ exprs/overlay.nix
@@ -2,4 +2,16 @@ final: prev: {
   wine-discord-ipc-bridge = prev.callPackage ./wine-discord-ipc-bridge.nix {
     inherit (prev.pkgsCross.mingw32) stdenv;
   };
+
+  fuzzel = prev.fuzzel.overrideAttrs (_: rec {
+    version = "1.10.2";
+
+    src = prev.fetchFromGitea {
+      domain = "codeberg.org";
+      owner = "dnkl";
+      repo = "fuzzel";
+      rev = version;
+      hash = "sha256-I+h93/I1Kra2S5QSi2XgICAVrcUmO9cmb8UttVuzjwg=";
+    };
+  });
 }
