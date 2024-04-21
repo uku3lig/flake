95b06db7752a1314db103b653f1b11de4f8e0ee3
uku <hi@uku.moe>
2024-04-21 10:24:03 +0200

    chore(overlay): remove fuzzel

--- exprs/overlay.nix
+++ exprs/overlay.nix
@@ -2,16 +2,4 @@ final: prev: {
   wine-discord-ipc-bridge = prev.callPackage ./wine-discord-ipc-bridge.nix {
     inherit (prev.pkgsCross.mingw32) stdenv;
   };
-
-  fuzzel = prev.fuzzel.overrideAttrs (_: rec {
-    version = "1.10.2";
-
-    src = prev.fetchFromGitea {
-      domain = "codeberg.org";
-      owner = "dnkl";
-      repo = "fuzzel";
-      rev = version;
-      hash = "sha256-I+h93/I1Kra2S5QSi2XgICAVrcUmO9cmb8UttVuzjwg=";
-    };
-  });
 }
