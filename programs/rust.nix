403292f8149eb6fb3313fd2942f52345c798dd53
uku <hi@uku.moe>
2024-05-20 23:55:56 +0200

    feat(programs): remove sscache from rust config

--- programs/rust.nix
+++ programs/rust.nix
@@ -7,10 +7,7 @@
   toml = pkgs.formats.toml {};
 in {
   hm.home.file.".cargo/config.toml".source = toml.generate "config.toml" {
-    build = {
-      rustc-wrapper = "${lib.getExe' pkgs.sccache "sccache"}";
-      target-dir = "${config.hm.home.homeDirectory}/.cargo/target";
-    };
+    build.target-dir = "${config.hm.home.homeDirectory}/.cargo/target";
 
     target.x86_64-unknown-linux-gnu = {
       linker = "${lib.getExe pkgs.clang}";
