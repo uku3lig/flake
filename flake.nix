27293a008917181762003d8f4303d5c3728516d7
uku <hi@uku.moe>
2024-06-09 17:36:31 +0200

    chore: reorder inputs alphabetically

--- flake.nix
+++ flake.nix
@@ -15,8 +15,27 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
-    # nix's most elaborate, overcomplicated joke
-    systems.url = "github:nix-systems/default";
+    agenix = {
+      url = "github:uku3lig/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.systems.follows = "systems";
+      inputs.home-manager.follows = "home-manager";
+      inputs.darwin.follows = "";
+    };
+
+    catppuccin.url = "github:Stonks3141/ctp-nix";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    deploy-rs = {
+      url = "github:serokell/deploy-rs";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.utils.follows = "flake-utils";
+      inputs.flake-compat.follows = "";
+    };
 
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
@@ -33,13 +52,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    catppuccin.url = "github:Stonks3141/ctp-nix";
-
     lanzaboote = {
       url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -50,20 +62,15 @@
       inputs.flake-compat.follows = "";
     };
 
-    agenix = {
-      url = "github:uku3lig/agenix";
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.systems.follows = "systems";
-      inputs.home-manager.follows = "home-manager";
-      inputs.darwin.follows = "";
-    };
-
-    deploy-rs = {
-      url = "github:serokell/deploy-rs";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.utils.follows = "flake-utils";
+      inputs.flake-utils.follows = "flake-utils";
       inputs.flake-compat.follows = "";
     };
+
+    # nix's most elaborate, overcomplicated joke
+    systems.url = "github:nix-systems/default";
 
     vscode-extensions = {
       url = "github:nix-community/nix-vscode-extensions";
@@ -76,13 +83,6 @@
       url = "github:nix-community/nixos-vscode-server";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
-    };
-
-    nixos-wsl = {
-      url = "github:nix-community/NixOS-WSL";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.flake-compat.follows = "";
     };
 
     # ==== uku3lig stuff ====
