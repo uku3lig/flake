cc9c169accfb00b392b05ee1fa666a8544047e2b
uku <hi@uku.moe>
2024-06-13 12:01:07 +0200

    feat(gnome): add tweaks and appindicator

--- programs/gnome.nix
+++ programs/gnome.nix
@@ -4,19 +4,24 @@
     displayManager.defaultSession = "gnome";
   };
 
-  environment.gnome.excludePackages =
-    (with pkgs; [
-      gnome-tour
-    ])
-    ++ (with pkgs.gnome; [
-      cheese # webcam tool
-      gnome-terminal
-      epiphany # web browser
-      geary # email reader
-      totem # video player
-      tali # poker game
-      iagno # go game
-      hitori # sudoku game
-      atomix # puzzle game
-    ]);
+  environment = with pkgs; {
+    systemPackages = [
+      gnome.gnome-tweaks
+      gnomeExtensions.appindicator
+    ];
+
+    gnome.excludePackages =
+      [gnome-tour]
+      ++ (with pkgs.gnome; [
+        cheese # webcam tool
+        gnome-terminal
+        epiphany # web browser
+        geary # email reader
+        totem # video player
+        tali # poker game
+        iagno # go game
+        hitori # sudoku game
+        atomix # puzzle game
+      ]);
+  };
 }
