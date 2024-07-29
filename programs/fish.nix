ca71ddac8c7a50e747b088cb7a8b51b48ac3391c
uku <hi@uku.moe>
2024-07-29 10:32:15 +0200

    fix(fish): test if ssh key file exists

--- programs/fish.nix
+++ programs/fish.nix
@@ -11,7 +11,9 @@
     enable = true;
 
     interactiveShellInit = with pkgs; ''
-      ssh-add -l | grep -q (ssh-keygen -lf ~/.ssh/id_ed25519) || ssh-add ~/.ssh/id_ed25519
+      if test -f ~/.ssh/id_ed25519
+        ssh-add -l | grep -q (ssh-keygen -lf ~/.ssh/id_ed25519) || ssh-add ~/.ssh/id_ed25519
+      end
 
       ${lib.getExe starship} init fish | source
       ${lib.getExe nix-your-shell} fish | source
