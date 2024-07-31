f8ed351d74f17fac00a0a59d41d3edfacc88d177
uku <hi@uku.moe>
2024-07-31 15:40:43 +0200

    feat(alacritty): switch to rosé pine

--- programs/alacritty.nix
+++ programs/alacritty.nix
@@ -5,8 +5,9 @@
 }: {
   hm.programs.alacritty = let
     theme = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/catppuccin/alacritty/ce476fb41f307d90f841c1a4fd7f0727c21248b2/catppuccin-macchiato.toml";
-      hash = "sha256-m0Y8OBD9Pgjw9ARwjeD8a+JIQRDboVVCywQS8/ZBAcc=";
+      # url = "https://raw.githubusercontent.com/catppuccin/alacritty/ce476fb41f307d90f841c1a4fd7f0727c21248b2/catppuccin-macchiato.toml";
+      url = "https://raw.githubusercontent.com/rose-pine/alacritty/3c3e36eb5225b0eb6f1aa989f9d9e783a5b47a83/dist/rose-pine.toml";
+      hash = "sha256-MheSmzz02ZLAOS2uaclyazu6E//eikcdFydFfkio0/U=";
     };
 
     themeAttr = builtins.fromTOML (builtins.readFile theme);
