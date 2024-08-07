0ff8bfef13a00598e411740676fc02958d59c658
uku <hi@uku.moe>
2024-08-07 10:08:25 +0200

    chore(overlay): bump jmusicbot to 0.4.3

--- exprs/overlay.nix
+++ exprs/overlay.nix
@@ -4,11 +4,11 @@ final: prev: {
   };
 
   jmusicbot = prev.jmusicbot.overrideAttrs (_: rec {
-    version = "0.4.2";
+    version = "0.4.3";
 
     src = prev.fetchurl {
       url = "https://github.com/jagrosh/MusicBot/releases/download/${version}/JMusicBot-${version}.jar";
-      hash = "sha256-Jg6/ju3ADBd7fc3njRzoEDVjIL4SzAzlTc02I4Q9hz4=";
+      hash = "sha256-7CHFc94Fe6ip7RY+XJR9gWpZPKM5JY7utHp8C3paU9s=";
     };
   });
 }
