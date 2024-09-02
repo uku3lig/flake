6ef9c54dc0f29d345e2e800de199378833797927
uku <hi@uku.moe>
2024-09-02 15:52:52 +0200

    feat(mottarone): add packages

--- systems/mottarone/default.nix
+++ systems/mottarone/default.nix
@@ -1,1 +1,9 @@
-{}
+{lib, pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    chromium
+    gtkterm
+    (pkgs.callPackage ./teams.nix {})
+  ];
+
+  services.resolved.dnsovertls = lib.mkForce "false";
+}
