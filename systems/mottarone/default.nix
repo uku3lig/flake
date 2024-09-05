2ddc486cf4c66c926b4dc4a69a540b2f59f37d4f
uku <hi@uku.moe>
2024-09-05 11:31:54 +0200

    chore(mottarone): more stuff

--- systems/mottarone/default.nix
+++ systems/mottarone/default.nix
@@ -10,8 +10,18 @@
     svn2git
   ];
 
+  i18n.defaultLocale = lib.mkForce "fr_FR.UTF-8";
+
   programs.git.package = pkgs.gitSVN;
-  hm.programs.git.enable = lib.mkForce false;
+  hm.programs = {
+    git.enable = lib.mkForce false;
+    ssh.enable = lib.mkForce false;
+  };
 
   services.resolved.dnsovertls = lib.mkForce "false";
+
+  services.tomcat = {
+    enable = true;
+    jdk = pkgs.temurin-bin-8;
+  };
 }
