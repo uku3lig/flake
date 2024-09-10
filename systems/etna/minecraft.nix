5069fa3f3dccd4c653ffa7d11b7786eb4487f211
uku <hi@uku.moe>
2024-09-10 16:43:29 +0200

    chore(etna): remove atm9

--- systems/etna/minecraft.nix
+++ systems/etna/minecraft.nix
@@ -9,21 +9,6 @@
 
   secret = _utils.setupSingleSecret config "minecraftEnv" {};
 
-  atm9 = _utils.mkMinecraftServer config {
-    name = "atm9";
-    port = 25565;
-    remotePort = 6004;
-    tag = "java17";
-    memory = "8G";
-    envFiles = [secret.path];
-    env = {
-      USE_AIKAR_FLAGS = "true";
-      MOD_PLATFORM = "AUTO_CURSEFORGE";
-      CF_SLUG = "all-the-mods-9";
-      CF_FILE_ID = "5458414";
-    };
-  };
-
   lynn = _utils.mkMinecraftServer config {
     name = "lynn";
     port = 25567;
@@ -40,7 +25,6 @@ in {
   imports = [
     secret.generate
 
-    atm9
     lynn
   ];
 
