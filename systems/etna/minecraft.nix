083bc84f3af4668ff6adee7fe7d814aa9d3b1391
uku <hi@uku.moe>
2024-09-30 15:24:31 +0200

    feat(etna): add ukuserv

--- systems/etna/minecraft.nix
+++ systems/etna/minecraft.nix
@@ -18,7 +18,7 @@
     env = {
       USE_AIKAR_FLAGS = "true";
       TYPE = "MODRINTH";
-      MODRINTH_MODPACK = "https://modrinth.com/modpack/adrenaserver/version/1.6.0+1.20.6.fabric";
+      MODRINTH_MODPACK = "https://modrinth.com/modpack/adrenaline/version/1.24.0+1.20.6.fabric";
     };
   };
 
@@ -34,12 +34,26 @@
       MODRINTH_MODPACK = "https://modrinth.com/modpack/adrenaline/version/1.24.0+1.21.1.fabric";
     };
   };
+
+  ukuserv = _utils.mkMinecraftServer config {
+    name = "ukuserv";
+    port = 25566;
+    remotePort = 6006;
+    memory = "4G";
+    envFiles = [secret.path];
+    env = {
+      USE_AIKAR_FLAGS = "true";
+      TYPE = "MODRINTH";
+      MODRINTH_MODPACK = "https://modrinth.com/modpack/adrenaline/version/1.24.0+1.21.1.fabric";
+    };
+  };
 in {
   imports = [
     secret.generate
 
     lynn
     vanilla
+    ukuserv
   ];
 
   systemd.services.restart-minecraft-servers = {
