792cb67dd26f89fdef73b1e8a9ccc4a6719e69b3
uku <hi@uku.moe>
2024-11-25 09:16:35 +0100

    chore(ghostty): change theme

--- programs/ghostty.nix
+++ programs/ghostty.nix
@@ -7,14 +7,11 @@
     packages = [ghostty.packages.${pkgs.system}.default];
 
     file.".config/ghostty/config".text = ''
-      theme = Ubuntu
+      theme = rose-pine
       font-family = Iosevka Nerd Font
       font-size = 12
       font-feature = -calt
       font-feature = -dlig
-
-      selection-foreground = #908caa
-      selection-background = #26233a
     '';
   };
 }
