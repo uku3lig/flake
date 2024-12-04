1f24ba2e7431b13537077c3760322d6d8bec29ba
uku <hi@uku.moe>
2024-12-04 15:47:30 +0100

    fix(ghostty): unset selection colors

--- programs/ghostty.nix
+++ programs/ghostty.nix
@@ -7,14 +7,11 @@
     packages = [ghostty.packages.${pkgs.system}.default];
 
     file.".config/ghostty/config".text = ''
-      theme = light:catppuccin-latte,dark:rose-pine
+      theme = light:catppuccin-latte,dark:catppuccin-mocha
       font-family = Iosevka Nerd Font
       font-size = 12
       font-feature = -calt
       font-feature = -dlig
-
-      selection-foreground = #908caa
-      selection-background = #26233a
     '';
   };
 }
