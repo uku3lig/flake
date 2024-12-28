e7b937da943b816b86f2a7db7ffbdbdc9475e370
uku <hi@uku.moe>
2024-12-28 17:38:17 +0100

    chore(dotnet): avoid using two different dotnet versions
    rider uses dotnet-sdk_8 for reshaper, but dotnetCorePackages has a
    different version

--- programs/dotnet.nix
+++ programs/dotnet.nix
@@ -12,8 +12,7 @@ in
     packages = [ pkgs.jetbrains.rider ];
 
     file = {
-      ".dotnet/8".source =
-        mkOutOfStoreSymlink "${pkgs.dotnetCorePackages.dotnet_8.sdk.unwrapped}/share/dotnet";
+      ".dotnet/8".source = mkOutOfStoreSymlink "${pkgs.dotnet-sdk_8}/share/dotnet";
       ".dotnet/mono".source = mkOutOfStoreSymlink pkgs.mono;
     };
   };
