113a6787e924f23cf201cd61e6c5c72c63860d52
uku <hi@uku.moe>
2025-01-20 19:27:47 +0100

    feat(etna): replace vanilla servers with nomi-ceu

--- systems/etna/minecraft.nix
+++ systems/etna/minecraft.nix
@@ -23,30 +23,16 @@ let
     };
   };
 
-  vanilla = _utils.mkMinecraftServer config {
-    name = "vanilla";
+  nomifactory = _utils.mkMinecraftServer config {
+    name = "nomi";
     port = 25565;
-    remotePort = 6005;
-    memory = "4G";
+    remotePort = 6007;
+    tag = "java8";
+    memory = "8G";
     envFiles = [ secret.path ];
     env = {
-      USE_AIKAR_FLAGS = "true";
-      TYPE = "MODRINTH";
-      MODRINTH_MODPACK = "https://modrinth.com/modpack/adrenaline/version/1.24.0+1.21.1.fabric";
-    };
-  };
-
-  ukuserv = _utils.mkMinecraftServer config {
-    name = "ukuserv";
-    port = 25566;
-    remotePort = 6006;
-    memory = "4G";
-    envFiles = [ secret.path ];
-    env = {
-      USE_AIKAR_FLAGS = "true";
-      TYPE = "MODRINTH";
-      MODRINTH_MODPACK = "https://modrinth.com/modpack/adrenaline/version/1.24.0+1.21.1.fabric";
-      MODRINTH_PROJECTS = "spark, no-chat-reports";
+      TYPE = "AUTO_CURSEFORGE";
+      CF_PAGE_URL = "https://www.curseforge.com/minecraft/modpacks/nomi-ceu/files/5821400";
     };
   };
 in
@@ -55,8 +41,7 @@ in
     secret.generate
 
     lynn
-    vanilla
-    ukuserv
+    nomifactory
   ];
 
   systemd.services.restart-minecraft-servers = {
