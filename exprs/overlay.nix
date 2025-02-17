cdc2928496c5dc4042ce56e3ee2c170239d9c4a6
uku <hi@uku.moe>
2025-02-17 15:09:39 +0100

    fix(exprs): bump esbuild to follow vencord

--- exprs/overlay.nix
+++ exprs/overlay.nix
@@ -18,10 +18,25 @@ inputs: final: prev: {
     env = old.env // {
       VENCORD_REMOTE = "Vendicated/Vencord";
       VENCORD_HASH = inputs.vencord.shortRev;
+
+      ESBUILD_BINARY_PATH = prev.lib.getExe (
+        prev.esbuild.overrideAttrs (
+          final: _: {
+            version = "0.25.0";
+            src = prev.fetchFromGitHub {
+              owner = "evanw";
+              repo = "esbuild";
+              rev = "v${final.version}";
+              hash = "sha256-L9jm94Epb22hYsU3hoq1lZXb5aFVD4FC4x2qNt0DljA=";
+            };
+            vendorHash = "sha256-+BfxCyg0KkDQpHt/wycy/8CTG6YBA/VJvJFhhzUnSiQ=";
+          }
+        )
+      );
     };
 
     pnpmDeps = old.pnpmDeps.overrideAttrs (_: {
-      outputHash = "sha256-ZUwtNtOmxjhOBpYB7vuytunGBRSuVxdlQsceRmeyhhI=";
+      outputHash = "sha256-0afgeJkK0OQWoqF0b8pHPMsiTKox84YmwBhtNWGyVAg=";
       outputHashAlgo = "sha256";
     });
   });
