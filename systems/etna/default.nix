b864fa9ffee9af08c5c406d50d29e8c0fa187703
uku <hi@uku.moe>
2025-03-22 00:52:49 +0100

    fix(systems/etna): temporarily add package override for cloudflared
    https://github.com/NixOS/nixpkgs/pull/390807

--- systems/etna/default.nix
+++ systems/etna/default.nix
@@ -1,11 +1,25 @@
 {
   lib,
+  pkgs,
   config,
   _utils,
   ...
 }:
 let
   tunnelId = "57f51ad7-25a0-45f3-b113-0b6ae0b2c3e5";
+
+  patchedBuildGoModule = pkgs.buildGoModule.override {
+    go = pkgs.buildPackages.go_1_22.overrideAttrs {
+      pname = "cloudflare-go";
+      version = "1.22.5-devel-cf";
+      src = pkgs.fetchFromGitHub {
+        owner = "cloudflare";
+        repo = "go";
+        rev = "af19da5605ca11f85776ef7af3384a02a315a52b";
+        hash = "sha256-6VT9CxlHkja+mdO1DeFoOTq7gjb3T5jcf2uf9TB/CkU=";
+      };
+    };
+  };
 
   secrets = _utils.setupSharedSecrets config { secrets = [ "frpToken" ]; };
   cfTunnelSecret = _utils.setupSingleSecret config "tunnelCreds" { };
@@ -75,6 +89,7 @@ in
 
     cloudflared = {
       enable = true;
+      package = pkgs.cloudflared.override { buildGoModule = patchedBuildGoModule; };
       tunnels.${tunnelId} = {
         credentialsFile = cfTunnelSecret.path;
         default = "http_status:404";
