80c28d4f3b191e975560d6f64515876826d72e02
uku <hi@uku.moe>
2025-04-03 15:12:05 +0200

    fix(systems/etna): unbreak cloudflared (the horrors)

--- systems/etna/default.nix
+++ systems/etna/default.nix
@@ -8,16 +8,27 @@
 let
   tunnelId = "57f51ad7-25a0-45f3-b113-0b6ae0b2c3e5";
 
-  patchedBuildGoModule = pkgs.buildGoModule.override {
-    go = pkgs.buildPackages.go_1_22.overrideAttrs {
-      pname = "cloudflare-go";
-      version = "1.22.5-devel-cf";
-      src = pkgs.fetchFromGitHub {
-        owner = "cloudflare";
-        repo = "go";
-        rev = "af19da5605ca11f85776ef7af3384a02a315a52b";
-        hash = "sha256-6VT9CxlHkja+mdO1DeFoOTq7gjb3T5jcf2uf9TB/CkU=";
-      };
+  patchedCloudflared = pkgs.cloudflared.override {
+    buildGoModule = pkgs.buildGoModule.override {
+      go = pkgs.buildPackages.go_1_23.overrideAttrs (old: {
+        pname = "cloudflare-go";
+        version = "1.22.5-devel-cf";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "cloudflare";
+          repo = "go";
+          rev = "af19da5605ca11f85776ef7af3384a02a315a52b";
+          hash = "sha256-6VT9CxlHkja+mdO1DeFoOTq7gjb3T5jcf2uf9TB/CkU=";
+        };
+
+        patches = map (
+          patch:
+          if (baseNameOf patch == "go_no_vendor_checks-1.23.patch") then
+            ../../exprs/go-no-vendor-1.22.patch
+          else
+            patch
+        ) old.patches;
+      });
     };
   };
 
@@ -89,7 +100,7 @@ in
 
     cloudflared = {
       enable = true;
-      package = pkgs.cloudflared.override { buildGoModule = patchedBuildGoModule; };
+      package = patchedCloudflared.overrideAttrs { meta.broken = false; };
       tunnels.${tunnelId} = {
         credentialsFile = cfTunnelSecret.path;
         default = "http_status:404";
