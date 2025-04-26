0ec3f9ef9cdd7d4d284cd7458f6889513fa53526
uku <hi@uku.moe>
2025-04-26 11:04:51 +0200

    chore: temporarily revert back to nix 2.24
    flake registry is bugged in 2.28

--- configs/common.nix
+++ configs/common.nix
@@ -95,7 +95,7 @@ in
   };
 
   nix = {
-    # package = pkgs.nixVersions.latest;
+    package = pkgs.nixVersions.nix_2_24;
     channel.enable = false;
     # The `flake:` syntax in `$NIX_PATH` seems to do some weird copying on Nix 2.24
     nixPath = [ "nixpkgs=${config.nixpkgs.flake.source}" ];
