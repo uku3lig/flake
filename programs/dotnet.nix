fb21b72c32a4eec51fa812161e0742145bb3c073
uku <hi@uku.moe>
2025-05-14 12:14:49 +0200

    fix(dotnet): recreate tmpfiles links

--- programs/dotnet.nix
+++ programs/dotnet.nix
@@ -3,7 +3,7 @@
   environment.systemPackages = [ pkgs.jetbrains.rider ];
 
   systemd.tmpfiles.rules = [
-    "L /opt/dotnet/8 - - - - ${pkgs.dotnet-sdk_8}/share/dotnet"
-    "L /opt/dotnet/mono - - - - ${pkgs.mono}"
+    "L+ /opt/dotnet/8 - - - - ${pkgs.dotnet-sdk_8}/share/dotnet"
+    "L+ /opt/dotnet/mono - - - - ${pkgs.mono}"
   ];
 }
