746fd9270167038f2d6350c94bceced8fb196c18
uku <hi@uku.moe>
2025-10-29 11:41:23 +0100

    fix(etna): dump pg databases separately

--- systems/etna/postgresql.nix
+++ systems/etna/postgresql.nix
@@ -1,4 +1,7 @@
 { config, pkgs, ... }:
+let
+  location = "/data/backups/postgresql";
+in
 {
   services = {
     postgresql = {
@@ -23,16 +26,43 @@
       '';
     };
 
-    postgresqlBackup = {
-      enable = true;
-      backupAll = true;
-      compression = "zstd";
-      location = "/data/backups/postgresql";
-      # default is -C, which is not an option in dumpall
-      # TODO: should probably backup databases individually
-      pgdumpOptions = "";
-    };
-
     borgbackup.jobs.postgresql = config.passthru.makeBorg "postgresql" "/data/backups/postgresql";
+  };
+
+  systemd = {
+    tmpfiles.rules = [ "d '${location}' 0700 postgres - - -" ];
+
+    services.postgresql-backup = {
+      description = "Backup of Postgresql databases";
+      requires = [ "postgresql.target" ];
+      startAt = "*-*-* 23:50:00";
+
+      path = [
+        pkgs.coreutils
+        config.services.postgresql.package
+      ];
+
+      script = ''
+        set -euo pipefail
+        umask 0077 # ensure backup is only readable by postgres user
+
+        IFS=$'\n' dbs=(`psql -tAc "SELECT datname FROM pg_database"`)
+
+        for database in "''${dbs[@]}"; do
+          if [[ ! "$database" =~ template* ]]; then
+            echo "dumping $database"
+            pg_dump -C "$database" > "${location}/$database.sql.tmp"
+            mv "${location}/$database.sql.tmp" "${location}/$database.sql"
+          fi
+        done
+
+        pg_dumpall --globals-only > "${location}/postgresql_global.sql"
+      '';
+
+      serviceConfig = {
+        Type = "oneshot";
+        User = "postgres";
+      };
+    };
   };
 }
