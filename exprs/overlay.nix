a31597a58aa501aa0d68b99fe740376904b4058a
uku <hi@uku.moe>
2025-11-24 10:02:42 +0100

    chore(exprs): update vencord

--- exprs/overlay.nix
+++ exprs/overlay.nix
@@ -17,25 +17,16 @@ inputs: final: prev: {
     version = "${old.version}+git.${inputs.vencord.shortRev}";
     src = inputs.vencord;
 
+    patches = [ ];
+    postPatch = "";
+
     env = old.env // {
       VENCORD_REMOTE = "Vendicated/Vencord";
       VENCORD_HASH = inputs.vencord.shortRev;
-      ESBUILD_BINARY_PATH = prev.lib.getExe (
-        prev.esbuild.overrideAttrs rec {
-          version = "0.25.1";
-          src = prev.fetchFromGitHub {
-            owner = "evanw";
-            repo = "esbuild";
-            rev = "v${version}";
-            hash = "sha256-vrhtdrvrcC3dQoJM6hWq6wrGJLSiVww/CNPlL1N5kQ8=";
-          };
-          vendorHash = "sha256-+BfxCyg0KkDQpHt/wycy/8CTG6YBA/VJvJFhhzUnSiQ=";
-        }
-      );
     };
 
     pnpmDeps = old.pnpmDeps.overrideAttrs {
-      outputHash = "sha256-5MjxEs+jbowJJbJ9+Z+vppFImpB+PZzEhntwRAgv+xM=";
+      outputHash = "sha256-M9yZxBtuZg5KwG2Sli+f6Ionwccq7F7tI8/FnP1iObA=";
     };
   });
 }
