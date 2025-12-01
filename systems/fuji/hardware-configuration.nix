0733e288d784057c7446eed109451206f924764b
uku <hi@uku.moe>
2025-12-01 18:45:50 +0100

    feat(fuji): switch to 7800X3D

--- systems/fuji/hardware-configuration.nix
+++ systems/fuji/hardware-configuration.nix
@@ -8,20 +8,22 @@
   modulesPath,
   ...
 }:
+
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
   boot.initrd.availableKernelModules = [
+    "nvme"
     "xhci_pci"
     "ahci"
-    "nvme"
+    "usb_storage"
     "usbhid"
     "sd_mod"
   ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
@@ -37,6 +39,10 @@
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/306D-3240";
     fsType = "vfat";
+    options = [
+      "fmask=0022"
+      "dmask=0022"
+    ];
   };
 
   fileSystems."/home" = {
@@ -44,22 +50,10 @@
     fsType = "ext4";
   };
 
-  fileSystems."/mnt/videos" = {
-    device = "/dev/disk/by-uuid/b7f70053-dda2-4084-bd92-ddeccb4b4950";
-    fsType = "btrfs";
-  };
-
   swapDevices = [
     { device = "/dev/disk/by-uuid/6ee8ec3d-3b26-4d6d-b43d-174f908fd8fe"; }
   ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
