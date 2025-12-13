c21e70ed231a32126472e9c0c6e8dc5be7c30785
uku <hi@uku.moe>
2025-12-13 23:23:05 +0100

    fix(vesuvio): update nitter

--- systems/vesuvio/nitter.nix
+++ systems/vesuvio/nitter.nix
@@ -1,16 +1,29 @@
 {
+  pkgs,
   config,
   _utils,
   ...
 }:
 let
   accounts = _utils.setupSingleSecret config "nitterAccounts" { };
+
+  nitter = pkgs.nitter.overrideAttrs {
+    version = "0-unstable-2025-12-08";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "zedeus";
+      repo = "nitter";
+      rev = "baeaf685d32098cd90ae1424cf8a19f3de9b0e2c";
+      hash = "sha256-y8cLFmqjNXRQramQ4SFgjJeoVOEBEuYctJ5/3vpySVY=";
+    };
+  };
 in
 {
   imports = [ accounts.generate ];
 
   services.nitter = {
     enable = true;
+    package = nitter;
     sessionsFile = accounts.path;
     server = {
       hostname = "nit.uku.moe";
