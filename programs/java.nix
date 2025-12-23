240c674814b86169ca9528112c136eee2f03cc30
uku <hi@uku.moe>
2025-12-23 18:29:44 +0100

    feat(java): add temurin 25

--- programs/java.nix
+++ programs/java.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   systemd.tmpfiles.rules = [
+    "L+ /opt/temurin-25 - - - - ${pkgs.temurin-bin-25}"
     "L+ /opt/temurin-21 - - - - ${pkgs.temurin-bin-21}"
     "L+ /opt/temurin-17 - - - - ${pkgs.temurin-bin-17}"
     "L+ /opt/temurin-8 - - - - ${pkgs.temurin-bin-8}"
