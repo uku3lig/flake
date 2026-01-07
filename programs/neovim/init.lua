78ad686ca19d4dea4542d283f3d7cc4e4a691a03
uku <hi@uku.moe>
2026-01-08 00:54:08 +0100

    fix(neovim): update for new nvim-treesitter

--- programs/neovim/init.lua
+++ programs/neovim/init.lua
@@ -106,13 +106,19 @@ require("catppuccin").setup({
 -- setup must be called before colorscheme
 vim.cmd.colorscheme("catppuccin")
 
-require("nvim-treesitter.configs").setup({
-	highlight = {
-		enable = true,
-		use_languagetree = true,
-	},
+-- tree-sitter, stolen from getchoo
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "*" },
+	callback = function(args)
+		local ft = args.match or vim.bo[args.buf].filetype
+		local language = vim.treesitter.language.get_lang(ft) or ft
 
-	indent = { enable = true },
+		if vim.treesitter.language.add(language) then
+			vim.treesitter.start(args.buf, language)
+			vim.bo[args.buf].syntax = "ON"
+			vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		end
+	end,
 })
 
 require("lualine").setup({
