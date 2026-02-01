58b1574cedb83ff8504adcf224e1077e50016b9f
uku <hi@uku.moe>
2026-02-01 14:13:45 +0100

    chore(etna): remove unplayed minecraft servers

--- systems/etna/minecraft.nix
+++ systems/etna/minecraft.nix
@@ -9,25 +9,10 @@ let
   inherit (config.virtualisation.oci-containers) backend;
 
   secret = _utils.setupSingleSecret config "minecraftEnv" { };
-
-  nomifactory = _utils.mkMinecraftServer config {
-    name = "nomi";
-    port = 25565;
-    remotePort = 6007;
-    tag = "java8";
-    memory = "8G";
-    envFiles = [ secret.path ];
-    env = {
-      TYPE = "AUTO_CURSEFORGE";
-      CF_PAGE_URL = "https://www.curseforge.com/minecraft/modpacks/nomi-ceu/files/5821400";
-    };
-  };
 in
 {
   imports = [
     secret.generate
-
-    nomifactory
   ];
 
   systemd.services.restart-minecraft-servers = {
