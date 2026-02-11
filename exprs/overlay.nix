4c516aeb46ad61561f66da1061162e033f37f3ab
uku <hi@uku.moe>
2026-02-11 13:01:31 +0100

    chore(exprs): remove waywall from overlay

--- exprs/overlay.nix
+++ exprs/overlay.nix
@@ -24,17 +24,4 @@ inputs: final: prev: {
 
     vendorHash = "sha256-vsfCgpilOHzJbTaJjJfMK/cSvtyFYJsPDjY4m3iuoFg=";
   };
-
-  waywall = prev.waywall.overrideAttrs (
-    f: p: {
-      version = "0.2026.01.11-unstable-2026-01-19";
-
-      src = prev.fetchFromGitHub {
-        owner = "tesselslate";
-        repo = "waywall";
-        rev = "0ba74cbd693fd0487912e40a274579410e5cc7e8";
-        hash = "sha256-26eax/Cg8cfIrJni9MEMwnjeSDjCy61g+Wk7sus/TXE=";
-      };
-    }
-  );
 }
