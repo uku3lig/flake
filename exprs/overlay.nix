6f8c22d9a44ef206940e8665f6951dc39923fbb4
uku <hi@uku.moe>
2026-02-24 09:24:56 +0100

    chore(overlay): fix picosvg build
    https://github.com/NixOS/nixpkgs/pull/493376

--- exprs/overlay.nix
+++ exprs/overlay.nix
@@ -3,7 +3,7 @@ inputs: final: prev: {
   pycharm-wrapped = prev.callPackage ./pycharm-wrapped.nix { };
 
   urbackup-client = prev.urbackup-client.overrideAttrs (old: {
-    nativeBuildInputs = old.nativeBuildInputs or [ ] ++ [ prev.autoreconfHook ];
+    nativeBuildInputs = old.nativeBuildInputs or [ ] ++ [ final.autoreconfHook ];
 
     patches = old.patches or [ ] ++ [ ./urbackup-client-install.patch ];
 
@@ -21,4 +21,19 @@ inputs: final: prev: {
       VENCORD_HASH = inputs.vencord.shortRev;
     };
   });
+
+  python3Packages = prev.python3Packages.overrideScope (
+    f: p: {
+      picosvg = p.picosvg.overrideAttrs {
+        patches = [
+          # Fix test failures with skia-pathops 0.9.x (m143)
+          # https://github.com/googlefonts/picosvg/pull/331
+          (final.fetchpatch {
+            url = "https://github.com/googlefonts/picosvg/commit/885ee64b75f526e938eb76e09fab7d93e946a355.patch";
+            hash = "sha256-fR3FfnEPHwSO1rMtmQEr1pyvByTx8T53FxSpuAKWIjw=";
+          })
+        ];
+      };
+    }
+  );
 }
