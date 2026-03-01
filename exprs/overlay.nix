8a1327a8099c998fe5620b491c0986d8c425b2df
uku <hi@uku.moe>
2026-03-02 00:03:39 +0100

    chore(exprs): remove picosvg overlay

--- exprs/overlay.nix
+++ exprs/overlay.nix
@@ -12,21 +12,6 @@ inputs: final: prev: {
     ];
   });
 
-  python3Packages = prev.python3Packages.overrideScope (
-    f: p: {
-      picosvg = p.picosvg.overrideAttrs {
-        patches = [
-          # Fix test failures with skia-pathops 0.9.x (m143)
-          # https://github.com/googlefonts/picosvg/pull/331
-          (final.fetchpatch {
-            url = "https://github.com/googlefonts/picosvg/commit/885ee64b75f526e938eb76e09fab7d93e946a355.patch";
-            hash = "sha256-fR3FfnEPHwSO1rMtmQEr1pyvByTx8T53FxSpuAKWIjw=";
-          })
-        ];
-      };
-    }
-  );
-
   jay = prev.jay.overrideAttrs (p: {
     version = "${p.version}+git.${inputs.jay.shortRev}";
     src = inputs.jay;
