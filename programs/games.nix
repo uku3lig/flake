fced52585a9ff2c0f27ba4e7a6e9e114f46e0f98
uku <hi@uku.moe>
2026-03-03 20:51:34 +0100

    feat(games): osu low latency

--- programs/games.nix
+++ programs/games.nix
@@ -1,4 +1,24 @@
 { pkgs, ... }:
+let
+  osu = pkgs.callPackage (
+    {
+      makeWrapper,
+      osu-lazer-bin,
+      symlinkJoin,
+    }:
+    symlinkJoin {
+      name = "osu-wrapped-${osu-lazer-bin.version}";
+      paths = [ osu-lazer-bin ];
+      nativeBuildInputs = [ makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/osu! \
+          --set SDL_VIDEODRIVER wayland \
+          --set PIPEWIRE_LATENCY 128/48000 \
+          --set PIPEWIRE_ALSA "{ alsa.format=S32_LE alsa.rate=48000 alsa.channels=2 alsa.buffer-bytes=1024 alsa.period-bytes=256 }"
+      '';
+    }
+  ) { };
+in
 {
   imports = [ ./mcsr ];
 
@@ -10,7 +30,7 @@
   environment.systemPackages = with pkgs; [
     avidemux
     krita
-    osu-lazer-bin
+    osu
     protonplus
     ruffle
 
