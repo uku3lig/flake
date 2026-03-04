d4b62e5234d2b812ae42d84db80c8dd96162f1d4
uku <hi@uku.moe>
2026-03-04 09:20:06 +0100

    chore(etna): fix zipline build

--- systems/etna/zipline.nix
+++ systems/etna/zipline.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   config,
   _utils,
   ...
@@ -12,6 +13,11 @@ in
 
   services.zipline = {
     enable = true;
+    package = pkgs.zipline.overrideAttrs (p: {
+      env = p.env // {
+        NODE_PATH = "${pkgs.node-gyp}/lib/node_modules";
+      };
+    });
     database.createLocally = true;
     environmentFiles = [ envFile.path ];
 
