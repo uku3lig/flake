90527ad498872ebcc36a894696808f3c4ef07d90
uku <hi@uku.moe>
2026-03-04 12:36:50 +0100

    fix(vesuvio): remove dhcp and enable ipv6

--- systems/vesuvio/default.nix
+++ systems/vesuvio/default.nix
@@ -4,8 +4,8 @@
     ./certificates.nix
     ./frp.nix
     ./gatus.nix
-    ./hetzner.nix
     ./mail
+    ./networking.nix
     ./nginx.nix
     ./nitter.nix
     ./pocket-id.nix
@@ -21,10 +21,24 @@
       ports = [ 4269 ];
       openFirewall = true;
     };
+
+    # Needed by the Hetzner Cloud password reset feature.
+    qemuGuest.enable = true;
+
+    # Hetzner DNS does not work with DoT
+    # resolved = {
+    #   dnsovertls = "false";
+    #   dnssec = "false";
+    # };
   };
 
   networking.firewall.allowedTCPPorts = [
     80
     443
   ];
+
+  # https://discourse.nixos.org/t/qemu-guest-agent-on-hetzner-cloud-doesnt-work/8864/2
+  systemd.services.qemu-guest-agent.path = [ pkgs.shadow ];
+
+  zramSwap.enable = true;
 }
