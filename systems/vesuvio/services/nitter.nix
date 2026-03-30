56f358f8c1aa28520086c22ca5ba25e230be77d2
uku <hi@uku.moe>
2026-03-30 17:13:16 +0200

    chore(vesuvio): update nitter

--- systems/vesuvio/services/nitter.nix
+++ systems/vesuvio/services/nitter.nix
@@ -1,16 +1,27 @@
 {
+  pkgs,
   config,
   _utils,
   ...
 }:
 let
   accounts = _utils.setupSingleSecret config "nitterAccounts" { };
+
+  nitter = pkgs.nitter.overrideAttrs (p: {
+    version = "0-unstable-2026-03-30";
+
+    src = p.src.overrideAttrs {
+      rev = "7d431781c37748faef5ed15bfa6b4865d875b99f";
+      hash = "sha256-LkGxQ7kvVNKXkYtDmpnwU3gbSO9caEcyUk8rnC2f81Y=";
+    };
+  });
 in
 {
   imports = [ accounts.generate ];
 
   services.nitter = {
     enable = true;
+    package = nitter;
     sessionsFile = accounts.path;
     server = {
       hostname = "nit.uku.moe";
