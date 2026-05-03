0aaeff858c92eea6693dc691d949b1db5c3ef6fb
uku <hi@uku.moe>
2026-05-03 17:05:58 +0200

    chore(etna): remove boner

--- systems/etna/minecraft.nix
+++ systems/etna/minecraft.nix
@@ -9,26 +9,10 @@ let
   inherit (config.virtualisation.oci-containers) backend;
 
   secret = _utils.setupSingleSecret config "minecraftEnv" { };
-
-  gtnhBoner = _utils.mkMinecraftServer config {
-    name = "gtnh-boner";
-    port = 25566;
-    remotePort = 6008;
-    tag = "java25";
-    memory = "8G";
-    envFiles = [ secret.path ];
-    env = {
-      TYPE = "GTNH";
-      GTNH_PACK_VERSION = "2.8.4";
-      MOTD = "GregTech New Horizons 2.8.4 \\u00A78(Big Boner™ Edition)";
-    };
-  };
 in
 {
   imports = [
     secret.generate
-
-    gtnhBoner
   ];
 
   # TODO: control per-server
