b4d38cc2b49f63e970e0983dc04fb86bd4234739
uku <hi@uku.moe>
2026-06-08 10:01:33 +0200

    chore(exprs): urbackup: 2.5.25 -> 2.5.30

--- exprs/overlay.nix
+++ exprs/overlay.nix
@@ -2,13 +2,22 @@ inputs: final: prev: {
   idea-wrapped = prev.callPackage ./idea-wrapped.nix { };
   pycharm-wrapped = prev.callPackage ./pycharm-wrapped.nix { };
 
-  urbackup-client = prev.urbackup-client.overrideAttrs (old: {
-    nativeBuildInputs = old.nativeBuildInputs or [ ] ++ [ final.autoreconfHook ];
+  urbackup-client = prev.urbackup-client.overrideAttrs (
+    f: p: {
+      version = "2.5.30";
 
-    patches = old.patches or [ ] ++ [ ./urbackup-client-install.patch ];
+      src = prev.fetchzip {
+        url = "https://hndl.urbackup.org/Client/${f.version}/urbackup-client-${f.version}.tar.gz";
+        sha256 = "sha256-Bm9GTIR0tyK0Y7uj7akQFK0phL+OCuqdky22iyCmF7o=";
+      };
 
-    configureFlags = old.configureFlags or [ ] ++ [
-      "--localstatedir=/var/lib"
-    ];
-  });
+      nativeBuildInputs = p.nativeBuildInputs or [ ] ++ [ final.autoreconfHook ];
+
+      patches = p.patches or [ ] ++ [ ./urbackup-client-install.patch ];
+
+      configureFlags = p.configureFlags or [ ] ++ [
+        "--localstatedir=/var/lib"
+      ];
+    }
+  );
 }
