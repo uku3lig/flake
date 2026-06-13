100523ecc1f55c2b364762bea7d45e466ad2c7ad
uku <hi@uku.moe>
2026-06-13 12:57:11 +0200

    feat(ghostty): add systemd service

--- programs/ghostty.nix
+++ programs/ghostty.nix
@@ -1,6 +1,11 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
+let
+  package = pkgs.ghostty;
+in
 {
-  environment.systemPackages = [ pkgs.ghostty ];
+  environment.systemPackages = [ package ];
+  systemd.packages = [ package ];
+  services.dbus.packages = [ package ];
 
   hj.".config/ghostty/config".text = ''
     font-family = Iosevka Term
@@ -9,4 +14,12 @@
     font-size = 12
     theme = light:Catppuccin Latte,dark:Catppuccin Mocha
   '';
+
+  systemd.user.services."app-com.mitchellh.ghostty" = {
+    restartIfChanged = false;
+    overrideStrategy = "asDropin";
+
+    # service "overrides" those, so we force them to null to keep nix dirs and whatnot
+    environment = lib.genAttrs [ "PATH" "LOCALE_ARCHIVE" "TZDIR" ] (_: lib.mkForce null);
+  };
 }
