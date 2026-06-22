8afc652d62b044db99fd5e6cd9d7fbeadd19a9c3
uku <hi@uku.moe>
2026-06-22 22:33:45 +0200

    feat(etna): switch jellyfin to postgresql

--- systems/etna/jellyfin.nix
+++ systems/etna/jellyfin.nix
@@ -1,6 +1,33 @@
-{
-  services.jellyfin = {
-    enable = true;
-    dataDir = "/data/jellyfin";
+{ config, camascaPkgs, ... }: {
+  services = {
+    jellyfin = {
+      enable = true;
+      dataDir = "/data/jellyfin";
+    };
+
+    postgresql = {
+      ensureDatabases = [ "jellyfin" ];
+      ensureUsers = [
+        {
+          name = "jellyfin";
+          ensureDBOwnership = true;
+        }
+      ];
+    };
+  };
+
+  systemd = {
+    services.jellyfin = {
+      path = [ config.services.postgresql.package ];
+      environment = {
+        POSTGRES_HOST = "/var/run/postgresql";
+        POSTGRES_PASSWORD = "";
+      };
+    };
+
+    tmpfiles.rules = [
+      "C+ ${camascaPkgs.jellyfin-pgsql}/lib/jellyfin-pgsql - - - - ${config.services.jellyfin.dataDir}/plugins/PostgreSQL"
+      "Z ${config.services.jellyfin.dataDir}/plugins/PostgreSQL - jellyfin jellyfin"
+    ];
   };
 }
