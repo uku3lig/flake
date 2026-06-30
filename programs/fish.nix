78e415ab09f237d74b8907f308ca5ff5c4bf4bdd
uku <hi@uku.moe>
2026-06-30 19:56:25 +0200

    feat(fish): replace hydro with my fork

--- programs/fish.nix
+++ programs/fish.nix
@@ -3,7 +3,16 @@
   programs.fish.enable = true;
 
   environment.systemPackages = with pkgs; [
-    fishPlugins.hydro
+    (fishPlugins.hydro.overrideAttrs {
+      version = "0-unstable-2026-06-30";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "uku3lig";
+        repo = "hydro";
+        rev = "294bbaf5394a7eddb874a05b28550da3a8963c0b";
+        hash = "sha256-OVOhoqdacKTj0muUXvymkRBm8hbLx7ZG7ns4HkOcrx0=";
+      };
+    })
   ];
 
   hj.".config/fish/config.fish".text = ''
@@ -16,6 +25,7 @@
       set --global hydro_color_git magenta
       set --global hydro_color_prompt green
       set --global hydro_color_duration yellow
+      set --global hydro_color_ssh c570fa
     end
   '';
 }
