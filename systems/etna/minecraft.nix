b47ec0fb3a437209cf3c94b65a583199480ad1f5
uku <hi@uku.moe>
2026-07-30 11:25:56 +0200

    feat(etna): add techopolis server

--- systems/etna/minecraft.nix
+++ systems/etna/minecraft.nix
@@ -9,16 +9,30 @@ let
   inherit (config.virtualisation.oci-containers) backend;
 
   secret = _utils.setupSingleSecret config "minecraftEnv" { };
+
+  techopolis = _utils.mkMinecraftServer config {
+    name = "techopolis";
+    port = 25565;
+    remotePort = 6007;
+    env = {
+      MODPACK_PLATFORM = "AUTO_CURSEFORGE";
+      CF_SLUG = "techopolis-3";
+      CF_FILE_ID = "8380837";
+      MOTD = "\\u00A76Techopolis 3 \\u00A78(12.3)\\u00A7r\\n\\u00A75#oucoupstroisligue";
+    };
+  };
 in
 {
   imports = [
     secret.generate
+
+    techopolis
   ];
 
   # TODO: control per-server
   systemd.services.restart-minecraft-servers = {
     wantedBy = [ "multi-user.target" ];
-    startAt = "*-*-* 12:00:00";
+    startAt = "*-*-* 05:00:00";
     restartIfChanged = false;
 
     script = "${lib.getExe' pkgs.systemd "systemctl"} restart ${backend}-mc-*.service";
