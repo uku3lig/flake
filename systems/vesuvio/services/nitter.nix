b28def07e42441d95d6e26751bd068c2876b1032
uku <hi@uku.moe>
2026-08-28 15:48:20 +0200

    chore(vesuvio): nitter override is back babyyyyyyyyyy

--- systems/vesuvio/services/nitter.nix
+++ systems/vesuvio/services/nitter.nix
@@ -1,16 +1,27 @@
 {
+  pkgs,
   config,
   _utils,
   ...
 }:
 let
   accounts = _utils.setupSingleSecret config "nitterAccounts" { };
+
+  nitter = pkgs.nitter.overrideNimAttrs (p: {
+    version = "0-unstable-2026-08-26";
+
+    src = p.src.overrideAttrs {
+      rev = "e4aefbc210a75d90d919fedba92f830c965ed989";
+      hash = "sha256-N1epzIKIcXqZMMBiq5eeACRF34hZMIP0q3R8S+kAjS8=";
+    };
+  });
 in
 {
   imports = [ accounts.generate ];
 
   services.nitter = {
     enable = true;
+    package = nitter;
     sessionsFile = accounts.path;
     server = {
       hostname = "nit.uku.moe";
